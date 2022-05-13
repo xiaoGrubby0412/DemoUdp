6e68c3a864bc40105c9c3a6d5aef26da5eabd079
wangman09 <wangman09@baidu.com>
2022-05-13 16:11:00 +0800

    commit udp demo project

---
+++ UpdProject/UpdProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UpdProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("按下任意按键开始发送...");
+            Console.ReadKey();
+
+            UdpClient client = new UdpClient();
+            IPAddress remoteIP = IPAddress.Parse("127.0.0.1");
+            int remotePort = 11000;
+            IPEndPoint remotePoint = new IPEndPoint(remoteIP, remotePort);
+
+            for (int i = 1; i < 50; i++)
+            {
+                string sendString = null;
+                sendString += "测试UDP 发送消息 " + i;
+
+                byte[] sendData = null;
+                sendData = Encoding.Default.GetBytes(sendString);
+
+                client.Send(sendData, sendData.Length, remotePoint);
+            }
+
+            client.Close();
+
+            Console.WriteLine("");
+            Console.WriteLine("数据发送成功, 按任意键退出...");
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ UpdProjectServer/UpdProjectServer/UpdProjectServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UpdProjectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1;
+            UdpClient client = new UdpClient(11000);
+            string receiveString = null;
+            byte[] receiveData = null;
+
+            IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("正在准备接收数据...");
+
+            while (true)
+            {
+                receiveData = client.Receive(ref remotePoint);
+                receiveString = Encoding.Default.GetString(receiveData);
+                Console.WriteLine(receiveString);
+                if (result == 50) 
+                {
+                    break;
+                }
+
+                result++;
+            }
+            client.Close();
+            Console.WriteLine("");
+            Console.WriteLine("数据接收完毕, 按任意建退出...");
+            System.Console.ReadKey();
+
+        }
+    }
+}
